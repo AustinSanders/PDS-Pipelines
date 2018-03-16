4395241c0babf42701af9149d7f0637578e9f29d
Austin Sanders <arsanders@usgs.gov>
2018-03-16 16:19:43 -0700

    Added initial batch DI find/enqueue

---
+++ bin/BatchDI.py
@@ -0,0 +1,37 @@
+import datetime
+import pytz
+import json
+from FindDI_Ready import archive_expired, volume_expired
+from db import db_connect
+
+
+def main():
+    PDS_info = json.load(open('/usgs/cdev/PDS/bin/PDSinfo.json','r'))
+    #reddis_queue = RedisQueue('DI_ReadyQueue')
+
+    try:
+        session, files, archives, _ = db_connect('pdsdi')
+    # @TODO Catch exceptions by type.  Bad practice to 'except Exception,' but
+    #   I don't know what exception could happen here.
+    except Exception as e:
+        print(e)
+        return 1
+
+    for target in PDS_info:
+        archive_id = PDS_info[target]['archiveid']
+        td = (datetime.datetime.now(pytz.utc)\
+            - datetime.timedelta(days=30)).strftime("%Y-%m-%d %H:%M:%S")
+        testing_date = datetime.datetime.strptime(str(td), "%Y-%m-%d %H:%M:%S")
+        expired = archive_expired(session, archive_id, files, testing_date)
+        if expired.count():
+            for target in expired:
+                print(target.filename)
+            print('Archive {} DI Ready: {} Files'.format(target, str(Q)))
+        else:
+            print('Archive {} DI Current'.format(target))
+    return 0
+
+        
+
+if __name__ == "__main__":
+    main()
