56c67c2633c417a532fbf6163766db190a0bbeb7
Austin Sanders <arsanders@usgs.gov>
2018-03-23 15:32:25 -0700

    Initial models.py

--- bin/models.py
+++ bin/models.py
@@ -1,78 +1,83 @@
 from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
-from datetime import time
 from functools import singledispatch, update_wrapper
-from sqlalchemy import Table, MetaData
+from sqlalchemy import Table, MetaData, Column, Integer, Time, String, Boolean
+from sqlalchemy import Float as Double
+from geoalchemy2 import Geometry
 
 Base = declarative_base(DeferredReflection)
 
-
-# @TODO find a way to use metadata without manual setup
-metadata = None
-def set_meta(meta):
-    metadata = meta
-
-def t_dispatch(func):
+def dispatch(func):
     """ Dispatch on obj, not type(obj)
     
     Decorator with functionality similar to singledispatch,
-    but dispatches based 'arg[0]' rather than 'type(arg[0])'.
+    but dispatches on 'arg[0]' rather than 'type(arg[0])'.
     This is necessary when the first object is specified as
     a type, not an instance of a type.
     """
     dispatcher = singledispatch(func)
     def wrapper(*args, **kw):
-        """ Custom wrapper returns first arg.  Must of of type 'type'"""
+        """ Custom wrapper returns first arg.  Must be of type 'type'"""
         return dispatcher.dispatch(args[0])(*args, **kw)
     wrapper.register = dispatcher.register
     update_wrapper(wrapper, func)
     return wrapper   
 
-def meta(keytype, **kwargs):
-    return _meta(_type(keytype), kwargs)
+# @TODO rename - currently named meta because all other classes
+#  are prefixed with 'meta' (meta_precision, meta_integer, etc.),
+#  but it doesn't make much sense as is.
+def meta(keytype, *args, **kwargs):
+    """ Returns a table based on the keytype description.
+    
+    Parameters
+    ----------
+    keytype : str
+        A type description.
+    *args
+        Variable length argument list.
+    **kwargs
+        Arbitrary keyword arguments.
 
-@t_dispatch
-def _meta(c_type, **kwargs):
+    Returns
+    -------
+    out : :class:`.meta_string` or :class:`.meta_precision` or :class:`.meta_integer` or :class:`.meta_time` or :class:`.meta_boolean`
+        A SQLAlchemy table with type specific column specification.
+    """
+    # Evaluate string type description to yield formal type
+    try:
+        return _meta(eval(keytype.capitalize()), *args, **kwargs)
+    except NameError:
+        print('Keytype {} not found\n'.format(keytype))
+        return None
+
+
+@dispatch
+def _meta(c_type, *args, **kwargs):
     """ If implementation for specified type doesn't exist..."""
     raise(NotImplementedError)
 
-@_meta.register(str)
+@_meta.register(String)
 def _(c_type, **kwargs):
-    return meta_string(kwargs)
+    return meta_string(**kwargs)
 
-@_meta.register(float)
+@_meta.register(Double)
 def _(c_type, **kwargs):
-    return meta_precision(kwargs)
+    return meta_precision(**kwargs)
 
-@_meta.register(int)
+@_meta.register(Integer)
 def _(c_type, **kwargs):
-    return meta_integer(kwargs)
+    return meta_integer(**kwargs)
 
-@_meta.register(time)
+@_meta.register(Time)
 def _(c_type, **kwargs):
-    return meta_time(kwargs)
+    return meta_time(**kwargs)
 
-@_meta.register(bool)
+@_meta.register(Boolean)
 def _(c_type, **kwargs):
-    return meta_boolean(kwargs)
-
-def _type(self, s_type):
-    """ Converts a string type description to a concrete type.
-    """
-    if s_type == 'string':
-        return str
-    elif s_type == 'double':
-        return float
-    elif s_type == 'integer':
-        return int
-    elif s_type == 'time':
-        return time
-    elif s_type == 'boolean':
-        return bool
-
-
+    return meta_boolean(**kwargs)
 
 # @TODO Replace autoloaded table declarations with expanded forms
-#   - We'll have to look at database / tables and make them by hand
+#   - We'll have to look at database schema and make the tables
+"""
 class datafiles(Base):
     __table__ = Table('datafiles', metadata, autoload=True)
 
@@ -84,44 +89,51 @@ class targets(Base):
 
 class keywords(Base):
     __table__ = Table('keywords', metadata, autoload=True)
+"""
 
 class meta_precision(Base):
     __tablename__ = 'meta_precision'
     upcid = Column(Integer, primary_key = True)
     typeid = Column(Integer)
-    # @TODO
-    value = Column()
+    # @TODO column description?
+    value = Column(Double)
 
 class meta_time(Base):
     __tablename__ = 'meta_time'
     upcid = Column(Integer, primary_key = True)
     typeid = Column(Integer)
-    # @TODO
-    value = Column()
+    # @TODO column description?
+    value = Column(Time)
 
 class meta_string(Base):
     __tablename__ = 'meta_string'
     upcid = Column(Integer, primary_key = True)
     typeid = Column(Integer)
-    # @TODO
-    value = Column()
+    # @TODO column description?
+    value = Column(String)
 
 class meta_integer(Base):
     __tablename__ = 'meta_integer'
     upcid = Column(Integer, primary_key = True)
     typeid = Column(Integer)
-    # @TODO
-    value = Column()
+    # @TODO column description?
+    value = Column(Integer)
 
 class meta_boolean(Base):
     __tablename__ = 'meta_boolean'
     upcid = Column(Integer, primary_key = True)
     typeid = Column(Integer)
-    # @TODO
-    value = Column()
+    # @TODO column description?
+    value = Column(Boolean)
 
 class meta_geometry(Base):
     __tablename__ = 'meta_geometry'
     upcid = Column(Integer, primary_key=True)
     typeid = Column(Integer)
     value = Column(Geometry('geometry'))
+
+if __name__ == "__main__":
+    t1 = meta('Boolean', upcid=1, typeid=0, value=True)
+    t2 = meta('string', upcid=2, typeid=1, value = 'StringExample')
+    t3 = meta('Integer', upcid=3, typeid=2, value = 1234)
+    t4 = meta('double', upcid=4, typeid=3, value = 5.6789)
