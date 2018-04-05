7301604a37bb4dc82b234a19698c0bd4bbf58cf1
Emilee Bovre <ebovre@claudius.wr.usgs.gov>
2018-04-05 15:17:22 -0700

    Numpy style documentation for DIQ

--- bin/DIqueueing.py
+++ bin/DIqueueing.py
@@ -25,18 +25,58 @@ from sqlalchemy import or_
 import pdb
 
 class Args:
+    """
+    Creates an object 'parser', which is populated by the attributes 
+    'archive', 'volume', and 'jobarray'. 
+
+    Attributes
+    ----------
+    archive : str
+    volume : str
+    jobarray : str
+
+    Methods
+    -------
+    _init_(self)
+        constructor
+    parse_args(self)
+        Converts argument strings into objects that are attributes
+        of the namespace
+    """ 
     def __init__(self):
         pass
 
     def parse_args(self):
+        """
+        Defines how to parse single command-line argument
 
+        Parameters
+        ----------
+        name : str 
+              '--archive'
+
+        dest : str
+            name of attribute to be added to parser
+        
+        required : str
+            makes optional arguments required
+
+        help
+            contains brief description of argument
+
+        Returns
+        -------
+        args : 
+            object with attributes 'archive', 'volume', and 'jobarray'
+        """
+        
         parser = argparse.ArgumentParser(description='PDS DI Data Integrity')
-
+        
         parser.add_argument('--archive', '-a', dest="archive", required=True,
                           help="Enter archive - archive to ingest")
 
         parser.add_argument('--volume', '-v', dest="volume",
-                          help="Enter voluem to Ingest")
+                          help="Enter volume to Ingest")
 
         parser.add_argument('--jobarray', '-j', dest="jobarray",
                           help="Enter string to set job array size")
@@ -49,8 +89,7 @@ class Args:
 
 def main():
 
-#    pdb.set_trace()
-
+    pdb.set_trace()
     args = Args()
     args.parse_args()
 
@@ -119,3 +158,4 @@ def main():
     
 if __name__ == "__main__":
     sys.exit(main())
+
