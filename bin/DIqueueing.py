6f6179ead759d5e357f8c3353f4911ba9a34fb78
Emilee Bovre <ebovre@claudius.wr.usgs.gov>
2018-05-07 12:51:20 -0700

    Docstrings added to DIqueueing

--- bin/DIqueueing.py
+++ bin/DIqueueing.py
@@ -55,7 +55,7 @@ class Args:
         required : str
             Makes optional arguments required
 
-        help
+        help : _helper
             Contains brief description of argument
 
         Returns
