2ae399bdc04bc95583a953d805f1d19d3aa9c62a
Austin Sanders <arsanders@usgs.gov>
2018-05-23 15:42:44 -0700

    Added configuration file for HPCJobber.py
    Moved cluster job parameters and logging setup to jobconfig.py
    Modified HPCjobber logic to remove branch logic for parameter
    injection
    
    Closes #50

--- bin/HPCjobber.py
+++ bin/HPCjobber.py
@@ -12,6 +12,7 @@ import logging
 from HPCjob import *
 
 import pdb
+from jobconfig import jobconfig, log_format
 
 
 class Args:
@@ -42,129 +43,37 @@ class Args:
 
 def main():
 
-    #    pdb.set_trace()
+    # pdb.set_trace()
 
     args = Args()
     args.parse_args()
 
-# ************* Logging Stuff ***********
+    # Grab the proper nested dict from config file
+    job = jobconfig[args.process]
 
-    if args.process == 'di':
-        logger = logging.getLogger('DIprocess_HPCjob')
-        logger.setLevel(logging.INFO)
-        logFileHandle = logging.FileHandler('/usgs/cdev/PDS/logs/DI.log')
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s, %(message)s')
-        logFileHandle.setFormatter(formatter)
-        logger.addHandler(logFileHandle)
-    elif args.process == 'checksumupdate':
-        logger = logging.getLogger('ChceksumUpdateprocess_HPCjob')
-        logger.setLevel(logging.INFO)
-        logFileHandle = logging.FileHandler('/usgs/cdev/PDS/logs/DI.log')
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s, %(message)s')
-        logFileHandle.setFormatter(formatter)
-        logger.addHandler(logFileHandle)
-    elif args.process == 'ingest':
-        logger = logging.getLogger('INGESTprocess_HPCjob')
-        logger.setLevel(logging.INFO)
-        logFileHandle = logging.FileHandler('/usgs/cdev/PDS/logs/Ingest.log')
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s, %(message)s')
-        logFileHandle.setFormatter(formatter)
-        logger.addHandler(logFileHandle)
-    elif args.process == 'thumbnail':
-        logger = logging.getLogger('Thumbnailprocess_HPCjob')
-        logger.setLevel(logging.INFO)
-        logFileHandle = logging.FileHandler('/usgs/cdev/PDS/logs/Process.log')
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s, %(message)s')
-        logFileHandle.setFormatter(formatter)
-        logger.addHandler(logFileHandle)
-    elif args.process == 'browse':
-        logger = logging.getLogger('Browseprocess_HPCjob')
-        logger.setLevel(logging.INFO)
-        logFileHandle = logging.FileHandler('/usgs/cdev/PDS/logs/Process.log')
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s, %(message)s')
-        logFileHandle.setFormatter(formatter)
-        logger.addHandler(logFileHandle)
-    elif args.process == 'projectionbrowse':
-        logger = logging.getLogger('ProjectionBrowseprocess_HPCjob')
-        logger.setLevel(logging.INFO)
-        logFileHandle = logging.FileHandler('/usgs/cdev/PDS/logs/Process.log')
-        formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s, %(message)s')
-        logFileHandle.setFormatter(formatter)
-        logger.addHandler(logFileHandle)
+    # Set up logging
+    logger = logging.getLogger(job['logger'])
+    logger.setLevel(logging.INFO)
+    logFileHandle = logging.FileHandler(job['handle'])
+    formatter = logging.Formatter(log_format)
+    logFileHandle.setFormatter(formatter)
+    logger.addHandler(logFileHandle)
+    logger.info(job['info'])
 
+    # Parametrize the HPC job using the configuration file
     date = datetime.datetime.now(pytz.utc).strftime("%Y%m%d%M")
     jobOBJ = HPCjob()
-    if args.process == 'di':
-        logger.info('Starting DI Process HPC Job Submission')
-        jobOBJ.setJobName('PDS_DIprocess')
-        jobOBJ.setStdOut('/usgs/cdev/PDS/output/DIprocess_%A_%a.out')
-        jobOBJ.setStdError('/usgs/cdev/PDS/output/DIprocess_%A_%a.err')
-        jobOBJ.setMemory('8192')
-        jobOBJ.setWallClock('240:00:00')
-        jobOBJ.setPartition('pds')
-        cmd = '/usgs/cdev/PDS/bin/DIprocess.py'
-        SBfile = '/usgs/cdev/PDS/output/DIhpc' + date + '.sbatch'
+    jobOBJ.setJobName(job['name'])
+    jobOBJ.setStdOut(job['stdout'])
+    jobOBJ.setStdError(job['stderr'])
+    jobOBJ.setMemory(job['memory'])
+    jobOBJ.setWallClock(job['wallclock'])
+    jobOBJ.setPartition(job['partition'])
 
-    elif args.process == 'checksumupdate':
-        logger.info('Starting Checksum Update Process HPC Job Submission')
-        jobOBJ.setJobName('PDS_CSUprocess')
-        jobOBJ.setStdOut('/usgs/cdev/PDS/output/CSUprocess_%A_%a.out')
-        jobOBJ.setStdError('/usgs/cdev/PDS/output/CSUprocess_%A_%a.err')
-        jobOBJ.setMemory('8192')
-        jobOBJ.setWallClock('20:00:00')
-        jobOBJ.setPartition('pds')
-        cmd = '/usgs/cdev/PDS/bin/ChecksumUpdateProcess.py'
-        SBfile = '/usgs/cdev/PDS/output/CSUhpc' + date + '.sbatch'
-
-    elif args.process == 'ingest':
-        logger.info('Starting INGEST process HPC Job Submission')
-        jobOBJ.setJobName('PDS_INGESTprocess')
-        jobOBJ.setStdOut('/usgs/cdev/PDS/output/INGESTprocess_%A_%a.out')
-        jobOBJ.setStdError('/usgs/cdev/PDS/output/INGESTprocess_%A_%a.err')
-        jobOBJ.setMemory('8192')
-        jobOBJ.setWallClock('240:00:00')
-        jobOBJ.setPartition('pds')
-        cmd = '/usgs/cdev/PDS/bin/IngestProcess.py'
-        SBfile = '/usgs/cdev/PDS/output/INGESThpc' + date + '.sbatch'
-
-    elif args.process == 'thumbnail':
-        logger.info('Starting Thumbnail process HPC Job Submission')
-        jobOBJ.setJobName('PDS_Thumbnailprocess')
-        jobOBJ.setStdOut('/usgs/cdev/PDS/output/Thumbnailprocess_%A_%a.out')
-        jobOBJ.setStdError('/usgs/cdev/PDS/output/Thumbnailprocess_%A_%a.err')
-        jobOBJ.setMemory('8192')
-        jobOBJ.setWallClock('20:00:00')
-        jobOBJ.setPartition('pds')
-        cmd = '/usgs/cdev/PDS/bin/thumbnail_process.py'
-        SBfile = '/usgs/cdev/PDS/output/Thpc' + date + '.sbatch'
-
-    elif args.process == 'browse':
-        logger.info('Starting Browse process HPC Job Submission')
-        jobOBJ.setJobName('PDS_Browseprocess')
-        jobOBJ.setStdOut('/usgs/cdev/PDS/output/Browseprocess_%A_%a.out')
-        jobOBJ.setStdError('/usgs/cdev/PDS/output/Browseprocess_%A_%a.err')
-        jobOBJ.setMemory('8192')
-        jobOBJ.setWallClock('20:00:00')
-        jobOBJ.setPartition('pds')
-        cmd = '/usgs/cdev/PDS/bin/browse_process.py'
-        SBfile = '/usgs/cdev/PDS/output/Bhpc' + date + '.sbatch'
-
-    elif args.process == 'projectionbrowse':
-        logger.info('Starting Projection Browse process HPC Job Submission')
-        jobOBJ.setJobName('PDS_ProjBrowseprocess')
-        jobOBJ.setStdOut('/usgs/cdev/PDS/output/ProjBrowseprocess_%A_%a.out')
-        jobOBJ.setStdError('/usgs/cdev/PDS/output/ProjBrowseprocess_%A_%a.err')
-        jobOBJ.setMemory('8192')
-        jobOBJ.setWallClock('20:00:00')
-        jobOBJ.setPartition('pds')
-        cmd = '/usgs/cdev/PDS/bin/projectionbrowse_process.py'
-        SBfile = '/usgs/cdev/PDS/output/Phpc' + date + '.sbatch'
+    # Date doesn't fit well with the recipe style config, so it includes
+    #  a @date@ tag that we replace with the current date
+    SBfile = job['SBfile'].replace('@date@', date)
+    cmd = job['cmd']
 
     if args.jobarray:
         JA = args.jobarray
@@ -187,7 +96,6 @@ def main():
         logger.info('Job Submission to HPC: Success')
     except IOError as e:
         logger.error('Jobs NOT Submitted to HPC')
-
 
 if __name__ == "__main__":
     sys.exit(main())
