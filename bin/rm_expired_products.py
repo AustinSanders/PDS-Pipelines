9a659d375ae38b7513d2e78653d5ee5fdade2a1a
Austin Sanders <arsanders@usgs.gov>
2018-06-12 15:12:46 -0700

    Updated to pull paths from config file.
    Renamed from rm_expired_pow to rm_expired_products.

--- bin/rm_expired_products.py
+++ bin/rm_expired_products.py
@@ -7,13 +7,15 @@ from models import clusterjobs_models
 from db import db_connect
 from sqlalchemy import and_
 
-def remove(paths, keys, id2task, session):
+import config as cfg
+
+def remove(path, keys, id2task, session):
     """ Removes a job from the supplied path based on its key.
     
     Parameters
     ----------
-    paths : list[String]
-        A list of string paths from which the keys will be deleted
+    path : String
+        The base path of pow/map2 files
     keys : list[sqlalchemy.collections.result(String, String)]
         Tuple of (typeid, key) where 'typeid' determines the path from
          which the key will be deleted and 'key' is the item to be
@@ -27,10 +29,11 @@ def remove(paths, keys, id2task, session):
     """
     for item in keys:
         typeid = int(item[0])
-        path = paths[id2task[typeid]]
+        f_path = os.path.join(path, id2task[typeid])
         key = item[1]
-        f = os.path.join(path, key)
-        """
+        f = os.path.join(f_path, key)
+        print(f)
+        # Remove file or directory matching the key and set purged in db
         try:
             if os.path.isfile(f):
                 os.remove(f)
@@ -39,8 +42,7 @@ def remove(paths, keys, id2task, session):
             set_purged(session, key)
         except OSError as e:
             print(e)
-            pass
-        """
+
 
 def set_purged(session, key):
     now = datetime.now()
@@ -51,9 +53,11 @@ def set_purged(session, key):
 
 def get_old_keys(session, n_days=14):
     cutoff = datetime.now() - timedelta(days=n_days)
+    #@TODO remove username
     old = session.query(clusterjobs_models.Processing.typeid, clusterjobs_models.Processing.key).filter(
         and_(clusterjobs_models.Processing.notified < cutoff,
-             clusterjobs_models.Processing.purged == None))
+             clusterjobs_models.Processing.purged == None,
+             clusterjobs_models.Processing.customerid == 1989))
     return old
 
 
@@ -64,21 +68,15 @@ def map_type_ids(session):
 
 
 def main():
-    paths = {'MAP2':'/pds_san/PDS_Services/MAP2/',
-             'POW':'/pds_san/PDS_Services/POW/'}
-
+    path = cfg.pow_map2_base
     session, _ = db_connect('clusterjob_prd')
-
     id2task = map_type_ids(session)
-
     try:
         n_days = sys.argv[1]
     except IndexError:
         n_days = 14
-
     old_files = get_old_keys(session)
-    remove(paths, old_files, id2task, session)
-
+    remove(path, old_files, id2task, session)
     return 0
 
 
