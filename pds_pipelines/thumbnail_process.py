e561f011667df7229f9d82a4b76a40846d67318b
Austin Sanders <arsanders@usgs.gov>
2018-08-06 15:05:50 -0700

    Pulled Bob's latest code + integrated with new style

--- pds_pipelines/thumbnail_process.py
+++ pds_pipelines/thumbnail_process.py
@@ -2,33 +2,43 @@
 import os
 import sys
 import pvl
+import json
+import datetime
+import pytz
 import logging
 from pysis import isis
 from pysis.exceptions import ProcessError
 
+from pysis.isis import getsn
+from ast import literal_eval
+
 from pds_pipelines.RedisQueue import *
 from pds_pipelines.Recipe import *
+from pds_pipelines.db import db_connect
+from pds_pipelines.models.upc_models import MetaString, DataFiles
+from pds_pipelines.models.pds_models import ProcessRuns, Files
+from pds_pipelines.config import pds_log_loc, pds_info_loc
 
 import pdb
 
+def getISISid(infile):
+    serial_num = getsn(from_=infile)
+    if isinstance(serial_num, bytes):
+        serial_num = serial_num.decode()
+    newisisSerial = serial_num.replace('\n', '')
+    return newisisSerial
 
-def scaleFactor(line, sample):
-    """
-    Parameters
-    ----------
-    line : int
-    sample : int
 
-    Returns
-    -------
-    int
-        scalefactor
-    """
+def scaleFactor(line, sample, jsonfile):
 
-    maxLine = 300
-    maxSample = 300
-    minLine = 50
-    minSample = 50
+#    pdb.set_trace()
+
+    infoDICT = json.load(open(jsonfile, 'r'))
+
+    maxLine = int(infoDICT['reduced']['thumbnail']['maxlines'])
+    maxSample = int(infoDICT['reduced']['thumbnail']['maxsamples'])
+    minLine = int(infoDICT['reduced']['thumbnail']['minlines'])
+    minSample = int(infoDICT['reduced']['thumbnail']['minsamples'])
 
     if sample < line:
         scalefactor = line/maxLine
@@ -40,94 +50,138 @@ def scaleFactor(line, sample):
     else:
         scalefactor = sample/maxSample
         testline = int(line/scalefactor)
-
+         
         if testline < minLine:
             scalefactor = line/minLine
 
+    if scalefactor < 1:
+        scalefactor = 1
     return scalefactor
 
 
 def makedir(inputfile):
-    """
-    Parameters
-    ----------
-    inputfile
-    
-    Returns
-    -------
-    str
-        finalpath
-    """
+#    pdb.set_trace()
 
     temppath = os.path.dirname(inputfile).lower()
-    finalpath = temppath.replace(
-        '/pds_san/pds_archive/', '/pds_san/PDS_Derived/UPC/images/')
+    finalpath = temppath.replace('/pds_san/pds_archive/', '/pds_san/PDS_Derived/UPC/images/')
 
     if not os.path.exists(finalpath):
         try:
-            os.makedirs(finalpath, 0755)
+            os.makedirs(finalpath, exist_ok=True)
         except OSError as e:
             if e.errno != errno.EEXIST:
                 raise
 
     return finalpath
 
+def DB_addURL(session, isisSerial, inputfile):
+    # pdb.set_trace()
+    newisisSerial = isisSerial.split(':')[0]
+    likestr = '%' + newisisSerial + '%'
+    Qobj = session.query(datafiles).filter(datafiles.isisid.like(likestr)).first()
+
+    if str(Qobj.isisid) == str(isisSerial):
+
+        outputfile = inputfile.replace('/pds_san/PDS_Derived/UPC/images/', '$thumbnail_server/')
+
+        DBinput = meta_string(upcid=Qobj.upcid,
+                            typeid='348',
+                            value=outputfile)
+
+        try:
+            session.add(DBinput)
+            session.commit()
+            return 'SUCCESS'
+        except:
+            return 'ERROR'
+
+
+def AddProcessDB(session, fid, outvalue):
+    # pdb.set_trace()
+    date = datetime.datetime.now(pytz.utc).strftime("%Y-%m-%d %H:%M:%S")
+
+    processDB = pds_models.ProcessRuns(fileid=fid,
+                             process_date=date,
+                             process_typeid='5',
+                             process_out=outvalue)
+
+    try:
+        session.merge(processDB)
+        session.commit()
+        return 'SUCCESS'
+    except:
+        return 'ERROR'
+
 
 def main():
 
-    #    pdb.set_trace()
+#    pdb.set_trace()
 
-    workarea = '/scratch/pds_services/workarea/'
+    # @TODO move to config file
+    workarea = '/home/arsanders/PDS-Pipelines/products/'
+    #workarea = '/scratch/pds_services/workarea/'
 
-# ***************** Set up logging *****************
+    ##***************** Set up logging *****************
     logger = logging.getLogger('Thumbnail_Process')
     logger.setLevel(logging.INFO)
-    logFileHandle = logging.FileHandler('/usgs/cdev/PDS/logs/Process.log')
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s, %(message)s')
+    logFileHandle = logging.FileHandler(pds_log_loc + 'Process.log')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s, %(message)s')
     logFileHandle.setFormatter(formatter)
-    logger.addHandler(logFileHandle)
+    logger.addHandler(logFileHandle)    
 
     RQ_main = RedisQueue('Thumbnail_ReadyQueue')
 
-    while int(RQ_main.QueueSize()) > 0:
+    PDSinfoDICT = json.load(open(pds_info_loc, 'r'))
 
-        inputfile = RQ_main.QueueGet()
+    # @TODO change to production servers
+    pds_session = db_connect('pdsdi_dev')
+    upc_session = db_connect('upcdev')
+
+    while int(RQ_main.QueueSize()) > 0:
+        item = literal_eval(RQ_main.QueueGet().decode("utf-8"))
+        inputfile = item[0]
+        fid = item[1]
+        archive = item[2]
         if os.path.isfile(inputfile):
             logger.info('Starting Process: %s', inputfile)
 
-            if 'Mars_Reconnaissance_Orbiter/CTX/' in inputfile:
-                mission = 'CTX'
-
-# ********** Derived DIR path Stuff **********************
-
-            finalpath = makedir(inputfile)
+            finalpath = makedir(inputfile)                  
 
             recipeOBJ = Recipe()
-            recip_json = recipeOBJ.getRecipeJSON(mission)
-            recipeOBJ.AddJsonFile(recip_json, 'pow')
-
-            infile = workarea + \
-                os.path.splitext(os.path.basename(inputfile))[
-                    0] + '.Tinput.cub'
-            outfile = workarea + \
-                os.path.splitext(os.path.basename(inputfile))[
-                    0] + '.Toutput.cub'
-
+            recip_json = recipeOBJ.getRecipeJSON(archive)
+            recipeOBJ.AddJsonFile(recip_json, 'reduced')
+            infile = workarea + os.path.splitext(os.path.basename(inputfile))[0] + '.Tinput.cub'
+            outfile = workarea + os.path.splitext(os.path.basename(inputfile))[0] + '.Toutput.cub'
             status = 'success'
             for item in recipeOBJ.getProcesses():
                 if status == 'error':
                     break
                 elif status == 'success':
                     processOBJ = Process()
-                    processR = processOBJ.ProcessFromRecipe(
-                        item, recipeOBJ.getRecipe())
+                    processR = processOBJ.ProcessFromRecipe(item, recipeOBJ.getRecipe())
 
                     if '2isis' in item:
                         processOBJ.updateParameter('from_', inputfile)
                         processOBJ.updateParameter('to', outfile)
                     elif item == 'spiceinit':
                         processOBJ.updateParameter('from_', infile)
+                    elif item == 'cubeatt':
+                        label = pvl.load(infile)
+                        bands = PDSinfoDICT[archive]['bandorder']
+                        query_bands = label['IsisCube']['BandBin'][PDSinfoDICT[archive]['bandbinQuery']]
+                        # Create a set from the list / single value
+                        try:
+                            query_band_set = set(query_bands)
+                        except:
+                            query_band_set = set([query_bands])
+                        
+                        # Iterate through 'bands' and grab the first value that is present in the
+                        #  set defined by 'bandbinquery' -- if not present, default to 1
+                        exband = next((band for band in bands if band in query_band_set), 1)
+
+                        band_infile = infile + '+' + str(exband)
+                        processOBJ.updateParameter('from_', band_infile)
+                        processOBJ.updateParameter('to', outfile)
 
                     elif item == 'ctxevenodd':
                         label = pvl.load(infile)
@@ -142,23 +196,24 @@ def main():
                         label = pvl.load(infile)
                         Nline = label['IsisCube']['Core']['Dimensions']['Lines']
                         Nsample = label['IsisCube']['Core']['Dimensions']['Samples']
-                        Sfactor = scaleFactor(Nline, Nsample)
+                        Nline = int(Nline)
+                        Nsample = int(Nsample)
+                        Sfactor = scaleFactor(Nline, Nsample, recip_json)
                         processOBJ.updateParameter('lscale', Sfactor)
                         processOBJ.updateParameter('sscale', Sfactor)
                         processOBJ.updateParameter('from_', infile)
                         processOBJ.updateParameter('to', outfile)
+
                     elif item == 'isis2std':
-                        outfile = finalpath + '/' + \
-                            os.path.splitext(os.path.basename(inputfile))[
-                                0] + '.thumbnail.jpg'
+                        final_outfile = finalpath + '/' + os.path.splitext(os.path.basename(inputfile))[0] + '.thumbnail.jpg'
                         processOBJ.updateParameter('from_', infile)
-                        processOBJ.updateParameter('to', outfile)
+                        processOBJ.updateParameter('to', final_outfile)
 
                     else:
                         processOBJ.updateParameter('from_', infile)
                         processOBJ.updateParameter('to', outfile)
 
-                    print processOBJ.getProcess()
+                    print(processOBJ.getProcess())
 
                     for k, v in processOBJ.getProcess().items():
                         func = getattr(isis, k)
@@ -169,16 +224,19 @@ def main():
                                 if '.cub' in outfile:
                                     os.rename(outfile, infile)
                             status = 'success'
+                            if '2isis' in item:
+                                isisSerial = getISISid(infile)
                         except ProcessError as e:
                             logger.error('Process %s :: Error', k)
                             status = 'error'
-
             if status == 'success':
+                testout = DB_addURL(upc_session, isisSerial, final_outfile)
                 os.remove(infile)
                 logger.info('Thumbnail Process Success: %s', inputfile)
+
+                AddProcessDB(pds_session, fid, 't')  
         else:
             logger.error('File %s Not Found', inputfile)
-
 
 if __name__ == "__main__":
     sys.exit(main())
