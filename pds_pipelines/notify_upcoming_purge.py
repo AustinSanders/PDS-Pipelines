86c4935dddf42ffc3f69d92e322d96bfd0f724ef
Austin Sanders <arsanders@usgs.gov>
2018-08-30 13:41:04 -0700

    Refactored email notifications into multiple scripts

--- pds_pipelines/notify_upcoming_purge.py
+++ pds_pipelines/notify_upcoming_purge.py
@@ -1,12 +1,10 @@
+from pds_pipelines.notify import setup_smtp, notify_upcoming_purge
 from pds_pipelines.db import db_connect
-from pds_pipelines.notify import setup_smtp, notify_finished, notify_error
-
 
 def main():
     session, _ = db_connect('clusterjob_prd')
     server = setup_smtp()
-    notify_finished(server, session)
-    notify_error(server, session)
+    notify_upcoming_purge(server, session)
 
 
 if __name__ == "__main__":
