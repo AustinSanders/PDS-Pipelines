afccea3f51556f1ef3d0cb855201dfa15f40b6af
Emilee Bovre <ebovre@claudius.wr.usgs.gov>
2018-09-06 14:46:44 -0700

    Removed hard coded URLs

--- pds_pipelines/FinalJobber.py
+++ pds_pipelines/FinalJobber.py
@@ -14,6 +14,8 @@ from collections import OrderedDict
 from pds_pipelines.RedisQueue import *
 from pds_pipelines.PDS_DBquery import *
 from pds_pipelines.HPCjob import *
+from pds_pipelines.config import pds_log,slurm_log,cmd_dir, scratch
+
 
 
 import pdb
@@ -25,7 +27,8 @@ def main():
     #***************** Setup Logging **************
     logger = logging.getLogger('FinalJobber')
     logger.setLevel(logging.INFO)
-    logFileHandle = logging.FileHandler('/usgs/cdev/PDS/logs/Service.log')
+    #logFileHandle = logging.FileHandler('/usgs/cdev/PDS/logs/Service.log')
+    logFileHandle = logging.FileHandler(pds_log+'Service.log')
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s, %(message)s')
     logFileHandle.setFormatter(formatter)
@@ -45,17 +48,20 @@ def main():
         logger.info('HPC Cluster job Submission Starting')
         jobOBJ = HPCjob()
         jobOBJ.setJobName(FKey + '_Final')
-        jobOBJ.setStdOut('/usgs/cdev/PDS/output/' + FKey + '_%A_%a.out')
-        jobOBJ.setStdError('/usgs/cdev/PDS/output/' + FKey + '_%A_%a.err')
+        #jobOBJ.setStdOut('/usgs/cdev/PDS/output/' + FKey + '_%A_%a.out')
+        #jobOBJ.setStdError('/usgs/cdev/PDS/output/' + FKey + '_%A_%a.err')
+        jobOBJ.setStdOut(slurm_log + FKey + '_%A_%a.out')
+        jobOBJ.setStdError(slurm_log + FKey + '_%A_%a.err')
         jobOBJ.setWallClock('24:00:00')
         jobOBJ.setMemory('8192')
         jobOBJ.setPartition('pds')
 
-        cmd = '/usgs/cdev/PDS/bin/ServiceFinal.py ' + FKey
+        cmd = cmd_dir+'ServiceFinal.py ' + FKey
         jobOBJ.setCommand(cmd)
         logger.info('HPC Command: %s', cmd)
 
-        SBfile = '/scratch/pds_services/' + FKey + '/' + FKey + '_final.sbatch'
+        #SBfile = '/scratch/pds_services/' + FKey + '/' + FKey + '_final.sbatch'
+        SBfile = scratch + FKey + '/' + FKey + '_final.sbatch'
         jobOBJ.MakeJobFile(SBfile)
 
         try:
