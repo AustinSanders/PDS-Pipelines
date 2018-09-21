4dd05d38cb45fb12717db54a0f6b9f9c5e15dfee
Emilee Bovre <emileebovre@Emilees-MacBook-Pro-2.local>
2018-09-20 23:00:42 -0700

    Added parse_args with input and output options

--- tools/Vold.py
+++ tools/Vold.py
@@ -2,6 +2,8 @@ import os
 import pvl
 import glob
 import json
+import argparse
+
 def load_pvl(pvl_file_path):
     with open(pvl_file_path, 'r') as f:
         f.readline()
@@ -9,21 +11,55 @@ def load_pvl(pvl_file_path):
     voldesc = pvl.loads(data)
     return voldesc
 
-def main():
-    s = input('File Path --> ')
-    filePath = glob.glob(s)
-    length = len(filePath)
-    vol_val = {}
-    print(length)
-    for n in range(length):
-        voldesc = load_pvl(filePath[n])
-        dataset_id = voldesc['VOLUME']['DATA_SET_ID']
-        volume_name = voldesc['VOLUME']['VOLUME_NAME']
+class Args:
+    def __init__(self):
+        pass
 
-        if isinstance(dataset_id, (list, tuple, set)):
-            vol_val[volume_name] = len(dataset_id)
-        else:
-            vol_val[volume_name]= 1
-            print(json.dumps(vol_val))
+    def parse_args(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--textfile','-f',help='Input text file path .txt')
+        parser.add_argument('--output','-o',help='Output file path .json')
+        args = parser.parse_args()
+        self.textfile = args.textfile
+        self.output = args.output
+
+def main():
+    args = Args()
+    args.parse_args()
+    
+    if args.texfile == str:
+        filePath = open(args.textfile,'r')
+        lines = filePath.readlines()
+        length = len(lines)
+        vol_val = {}
+        for n in range(length):
+            voldesc = load_pvl(lines[n].rstrip())
+            dataset_id = voldesc['VOLUME']['DATA_SET_ID']
+            volume_name = voldesc['VOLUME']['VOLUME_NAME']
+            if isinstance(dataset_id, (list, tuple, set)):
+                vol_val[volume_name] = len(dataset_id)
+            else:
+                vol_val[volume_name]= 1
+    else:
+        filePath = open(args.singlefile,'r')
+        lines = filePath.readlines()
+        length = len(lines)
+        vol_val = {}
+        for n in range(length):
+            voldesc = load_pvl(lines[n].rstrip())
+            dataset_id = voldesc['VOLUME']['DATA_SET_ID']
+            volume_name = voldesc['VOLUME']['VOLUME_NAME']
+            if isinstance(dataset_id, (list, tuple, set)):
+                vol_val[volume_name] = len(dataset_id)
+            else:
+                vol_val[volume_name]= 1
+
+    
+    if type(args.output) == str:
+        f = open(args.output,'w')
+        f.write(str(json.dumps(vol_val)))
+    else:
+        print(json.dumps(vol_val))
+
 if __name__ == '__main__':
     main()
