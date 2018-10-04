ddf8a0d9a11dba2c4476a16c53982a514aa71252
Emilee Bovre <36746206+eabovre@users.noreply.github.com>
2018-10-04 13:23:10 -0700

    Add tool (vold.py) to inspect voldesc.cat files
    * Sync pds2010mgr commits with dev (#138)
    
    * Fixed args to FindDI_Ready and path for UPCprocess
    
    * Added upper/lower case handling for softlinks
    
    * Removed browse/thumb queueing
    
    * Fix Issue #139
    
    In https://github.com/USGS-Astrogeology/PDS-Pipelines/blob/18249d9fb80983559bbd730cbda6d006d4a119f1/pds_pipelines/Ingestqueueing.py#L81 changed "archivepath" to "args.archive"
    
    * Fixed DI Processing bug (#143)
    
    * Fixed args to FindDI_Ready and path for UPCprocess
    
    * Added upper/lower case handling for softlinks
    
    * Removed browse/thumb queueing
    
    * Added session.merge for UPC process errors
    
    * Moved Ingestqueueing back to pds_pipelines directory
    
    * Dev (#141)
    
    * Sync pds2010mgr commits with dev (#138)
    
    * Fixed args to FindDI_Ready and path for UPCprocess
    
    * Added upper/lower case handling for softlinks
    
    * Removed browse/thumb queueing
    
    * Fix Issue #139
    
    In https://github.com/USGS-Astrogeology/PDS-Pipelines/blob/18249d9fb80983559bbd730cbda6d006d4a119f1/pds_pipelines/Ingestqueueing.py#L81 changed "archivepath" to "args.archive"
    
    * Added session.merge for UPC process errors
    
    * Moved Ingestqueueing back to pds_pipelines directory
    
    * Removed extra QueueGet() call that was causing bug.
    
    * Add shebang line (#146)
    
    * Removed print statements in favor of logging
    
    * Added shebang line
    
    * Issue #148 (#150)
    
    * Added 'choices' to args from jobconfig.py
    
    * Removed print statement
    
    * Changed 'choices' to list(jobconfig,keys))
    
    * help string changed to .format(choices)
    
    * Added 'tools' directory
    
    * Added parse_args with input and output options
    
    * Fixing up arguments
    
    * Added some URL abilities, still more to go
    
    * Trying sys.argv, still unsure how to make it optional
    
    * URL is not easy
    
    * Finished Issue #79
    
    * Deleted import glob, still working on fixing the repeated line
    
    * Changed 'vol_val' to ds_count(voldescPvl), still need to fix iterator
    
    * Fixed iteration issue, should be now complete
    
    * Finally I think I am done. Tests passed.

---
+++ tools/Vold.py
@@ -0,0 +1,77 @@
+import os, sys
+import pvl
+import json
+import argparse
+import urllib.request
+
+
+def load_pvl(pvl_file_path):
+    with open(pvl_file_path, 'r') as f:
+        f.readline()
+        data = f.read()
+    voldesc = pvl.loads(data)
+    return voldesc
+
+class Args:
+    def __init__(self):
+        pass
+
+    def parse_args(self):
+        parser = argparse.ArgumentParser()
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument('local', nargs = '?', help='Input local Voldesc File path or URL (point to a single Voldesc file)')
+        group.add_argument('--textfile','-f',help='Input text file path (file should contain a list of paths or URLs)')
+        parser.add_argument('--output','-o',help='Output file path (outputs to json)')
+        args = parser.parse_args()
+        self.local = args.local
+        self.textfile = args.textfile
+        self.output = args.output
+
+def ds_count(voldescPvl):
+    args = Args()
+    args.parse_args()
+    vol_val = {}
+    dataset_id = voldescPvl['VOLUME']['DATA_SET_ID']
+    volume_name = voldescPvl['VOLUME']['VOLUME_NAME']
+    if isinstance(dataset_id, (list, tuple, set)):
+        vol_val[volume_name] = len(dataset_id)
+    else:
+        vol_val[volume_name]= 1
+    return vol_val
+    
+def main():
+    args = Args()
+    args.parse_args()
+    a = {}
+
+    if args.textfile is not None:
+        filePath = open(args.textfile,'r')
+        lines = filePath.readlines()
+        length = len(lines)
+        
+        for n in range(length):
+            if 'https' in lines[n] or 'http' in lines[n] or 'ftp' in lines[n]:
+                voldesc = urllib.request.urlopen(lines[n])
+                voldescPvl = pvl.load(voldesc)     
+            else:
+                voldescPvl = load_pvl(lines[n].rstrip())
+            a.update(ds_count(voldescPvl))    
+    else:
+        if 'https' in args.local or 'http' in args.local or 'ftp' in args.local:
+            voldesc = urllib.request.urlopen(args.local)
+            voldescPvl = pvl.load(voldesc)
+        else:
+            filePath = open(str(args.local), 'r')
+            voldescPvl = load_pvl(str(args.local))
+        a.update(ds_count(voldescPvl))
+    
+
+
+    if args.output is not None:
+        f = open(args.output,'w')
+        f.write(str(json.dumps(a)))
+    else:
+        print(json.dumps(a))
+
+if __name__ == '__main__':
+    main()
