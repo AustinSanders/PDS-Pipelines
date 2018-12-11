0d35582ec0f88a875e6c81697fa7c2d05eb2c47d
USGSsakins <sakins@usgs.gov>
2018-12-11 11:23:50 -0700

    Initial checkin of dashboard JSON

---
+++ pds_pipelines/dashboard.py
@@ -0,0 +1,20 @@
+import redis
+import json
+from pds_pipelines.config import redis_info as ri
+from pds_pipelines.config import default_namespace
+
+redis_queues = {'DI':'DI_ReadyQueue', 'Browse':'Browse_ReadyQueue', 'UPC':'UPC_ReadyQueue', 'Thumbnail':'Thumbnail_ReadyQueue', 'Ingest':'Ingest_ReadyQueue', 'Pilot': 'PilotB_ReadyQueue'}
+redis_keys = {'NFS':'nfs_load'}
+
+rdb = redis.StrictRedis(host=ri['host'], port=ri['port'], db=ri['db'])
+
+status = {}
+
+for key, value in redis_queues:
+    queue_name = '%s:%s' % (default_namespace, value)
+    status[key] = rdb.llen(queue_name)
+
+for key, value in redis_keys:
+    status[key] = rdb.llen(value)
+
+json.dumps(status)
