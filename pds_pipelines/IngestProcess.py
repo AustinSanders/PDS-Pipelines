dcf0d50e39a6fea390d36cb52b84495bee8c7951
David P. Mayer <dpmayer@usgs.gov>
2019-03-04 09:37:07 -0700

    Fix argparse typo

--- pds_pipelines/IngestProcess.py
+++ pds_pipelines/IngestProcess.py
@@ -28,7 +28,7 @@ class Args(object):
         parser.set_defaults(override=False)
 
         parser.add_argument('--log', '-l', dest="log_level",
-                            choice=['DEBUG', 'INFO',
+                            choices=['DEBUG', 'INFO',
                                     'WARNING', 'ERROR', 'CRITICAL'],
                             help="Set the log level.", default='INFO')
 
