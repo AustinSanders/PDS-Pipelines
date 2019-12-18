4dd492967dbf9d44cca9aca215b6966691b305a5
Adam Paquette <acpaquette@usgs.gov>
2019-12-17 18:02:07 -0700

    Added dummy config file

---
+++ pds_pipelines/config.py
@@ -0,0 +1,11 @@
+import os
+# Database credentials
+credentials = {'test': {'user': 'upcmgr',
+                        'pass': '',
+                        'host': 'localhost',
+                        'port': '5432',
+                        'db': 'upc'}
+               }
+
+# Redis path(?) info
+redis_info = {'host': 'localhost', 'port': '6379', 'db': '0'}
