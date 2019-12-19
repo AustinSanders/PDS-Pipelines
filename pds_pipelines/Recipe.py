ab68392f1f9042695e340af2ce629de63dbf4612
Kaitlyn Lee <kdl222@nau.edu>
2019-12-19 14:36:06 -0700

    Updated doc strings.

--- pds_pipelines/Recipe.py
+++ pds_pipelines/Recipe.py
@@ -15,6 +15,7 @@ from pds_pipelines.config import recipe_base
 
 class Recipe(Process):
     """
+    
     Parameters
     ----------
     Process
@@ -29,11 +30,14 @@ class Recipe(Process):
         self.recipe = []
 
     def AddJsonFile(self, file, proc):
-        """
+        """ Adds a recipe JSON dictionary to the recipe list.
+
         Parameters
         ----------
         file : str
+            The name of the JSON to load.
         proc : str
+            The process pipeline.
         """
 
         # @TODO use 'with' statement for file read
@@ -49,31 +53,36 @@ class Recipe(Process):
             self.recipe.append(processDict)
 
     def addMissionJson(self, mission, proc):
-        """
+        """ Adds a recipe JSON for a specific mission.
+
         Parameters
         ----------
         mission : str
+            The name of the mission.
         proc : str
-
+            The process pipeline.
         """
+
         recipe_json = recipe_base + mission + '.json'
         self.AddJsonFile(recipe_json, proc)
 
     def getRecipe(self):
-        """
+        """ Returns list of recipe dictionaries.
+
         Returns
         -------
         list
-            self.recipe
+            Recipe dictionaries.
         """
         return self.recipe
 
     def getProcesses(self):
-        """
+        """ Returns list of process names.
+
         Returns
         -------
         list
-            processList
+            List of process names.
         """
 
         processList = []
@@ -84,18 +93,22 @@ class Recipe(Process):
         return processList
 
     def AddProcess(self, process):
-        """
+        """ Adds process to recipe.
+
         Parameters
         ----------
         process : str
+            The name of the process pipeline.
         """
         self.recipe.append(process)
 
     def TestgetStep(self, file):
-        """
+        """ 
+
         Parameters
         ----------
-        file : str
+        file : str 
+            The name of the JSON to load.
 
         Returns
         -------
