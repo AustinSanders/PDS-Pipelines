ba5a1c7cafd502ae8f00634a8d089dfa42ccb3d6
Austin Sanders <arsanders@usgs.gov>
2019-12-20 10:42:20 -0700

    Added histogram summary query

--- pds_pipelines/write_view_jsons.py
+++ pds_pipelines/write_view_jsons.py
@@ -14,11 +14,30 @@ queries = {
         GROUP by d.instrumentid, i.instrument, i.mission, i.spacecraft, d.targetid, t.targetname, t.system, i.displayname
         """,
     'band_summary': """
-        SELECT DISTINCT i.instrumentid, i.instrument, j.jsonkeywords -> 'caminfo' -> 'isislabel' -> 'isiscube' -> 'bandbin' ->> 'filtername' AS filtername, 
+        SELECT DISTINCT i.instrumentid, i.instrument, j.jsonkeywords -> 'caminfo' -> 'isislabel' -> 'isiscube' -> 'bandbin' ->> 'filtername' AS filtername,
         j.jsonkeywords -> 'caminfo' -> 'isislabel' -> 'isiscube' -> 'bandbin' -> 'center' ->> 0 AS center
         FROM instruments i
         JOIN datafiles d on (d.instrumentid = i.instrumentid)
         JOIN json_keywords j on (d.upcid = j.upcid)
+        """,
+    'histogram_summary': """
+        SELECT s.instrumentid, s.targetid, s.stat_type, width_bucket(s.val, 0 , 360, 20) AS bucket,
+    		((width_bucket(s.val, 0 , 360, 20) -1) * 18) ||
+    		' - ' ||
+    		(width_bucket(s.val, 0 , 360, 20) * 18) ||
+    		' degrees' AS bucket_range, count(*) AS total
+
+        FROM (SELECT t.instrumentid, t.targetid, v.*
+        	  FROM search_terms t,
+        	  LATERAL (values('solar_longitude', t.solarlongitude),
+        					  ('maximumphase', t.maximumphase),
+        					  ('minimumphase', t.minimumphase),
+        					  ('maximumemission', t.maximumemission),
+        					  ('minimumemission', t.minimumemission),
+        					  ('maximumincidence', t.maximumincidence),
+        					  ('minimumincidence', t.minimumincidence)) v (stat_type, val)) AS s WHERE s.val IS NOT NULL
+        GROUP BY s.instrumentid, s.targetid, s.stat_type, bucket
+        ORDER BY s.instrumentid, s.targetid, s.stat_type;
         """
 }
 
@@ -27,7 +46,7 @@ def parse_args():
 
     parser.add_argument('--path', '-p', dest="path", required=False,
                         help="Enter path - where to write the JSONs.")
-    
+
     args = parser.parse_args()
     return args
 
@@ -39,7 +58,7 @@ def main(user_args):
         path = view_path
 
     session, _ = db_connect(upc_db)
-    
+
     for key in queries:
         json_query = "with t AS ({}) SELECT json_agg(t) FROM t;".format(queries[key])
         output = session.execute(json_query)
