c99725c696ba4dce6844eb4a0efd52638ceacac2
Kaitlyn Lee <kdl222@nau.edu>
2020-04-23 14:10:58 -0700

    Updated code to get tests to pass

--- pds_pipelines/upc_process.py
+++ pds_pipelines/upc_process.py
@@ -68,12 +68,8 @@ def getISISid(infile):
     newisisSerial : str
         The serial number of the input file.
     """
-    try:
-        serial_num = available_modules['isis'].getsn(from_=infile)
-    except (ProcessError, KeyError) as e:
-        # If either isis was not imported or a serial number could not be
-        # generated from the infile set the serial number to an empty string
-        serial_num = ''
+    serial_num = available_modules['isis'].getsn(from_=infile)
+
     # in later versions of getsn, serial_num is returned as bytes
     if isinstance(serial_num, bytes):
         serial_num = serial_num.decode()
@@ -262,7 +258,10 @@ def create_datafiles_record(label, edr_source, input_cube, session_maker):
     # Attemp to get the ISIS serial from the cube
     try:
         isis_id = getISISid(input_cube)
-    except:
+        
+    except(ProcessError, KeyError):
+        # If either isis was not imported or a serial number could not be
+        # generated from the infile set the serial number to an empty string
         isis_id = None
 
     datafile_attributes['isisid'] = isis_id
