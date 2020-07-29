efa93d4dae102436ef1a6b3dec544fbe28a1d9c0
Kaitlyn Lee <kdl222@nau.edu>
2020-07-29 09:54:57 -0700

    Updated config file (#455)
    * Updated config file
    
    * Added warning when checksums do not match (#454)

--- pds_pipelines/config.py
+++ pds_pipelines/config.py
@@ -1,22 +1,23 @@
 import os
 from pathlib import Path
+
 # Database credentials
 credentials = {'upc_test': {'user': 'postgres',
-                        'pass': '',
-                        'host': 'localhost',
-                        'port': '5432',
-                        'db': 'upc_test'},
-               'di_test': {'user': 'postgres',
                             'pass': '',
                             'host': 'localhost',
                             'port': '5432',
-                            'db': 'di_test'},
+                            'db': 'upc_test'},
+               'di_test': {'user': 'postgres',
+                           'pass': '',
+                           'host': 'localhost',
+                           'port': '5432',
+                           'db': 'di_test'},
                'clusterjob_test': {'user': 'postgres',
-                                  'pass': '',
-                                  'host': 'localhost',
-                                  'port': '5432',
-                                  'db': 'clusterjobs_test'}
-               }
+                                   'pass': '',
+                                   'host': 'localhost',
+                                   'port': '5432',
+                                   'db': 'clusterjobs_test'}
+              }
 
 # Redis path(?) info
 redis_info = {'host': 'localhost', 'port': '6379', 'db': '0'}
@@ -33,26 +34,20 @@ link_dest = '/pds_san/PDS_Archive_Links/'
 # Uses the root of the project to set the base path for all necessary files
 root = Path(__file__).parent.parent
 recipe_base = os.path.join(root, 'recipe/new/')
-
 pds_info = os.path.join(root, 'pds_pipelines/PDSinfo.json')
-
-pds_log = '/home/acpaquette/repos/PDS-Pipelines/logs/'
-
-slurm_log = '/home/acpaquette/repos/PDS-Pipelines/output/'
-
+pds_log = os.path.join(root, 'logs/')
+slurm_log = os.path.join(root, 'output/')
 cmd_dir = os.path.join(root, 'pds_pipelines/')
-
 keyword_def = os.path.join(root, 'recipe/Keyword_Definition.json')
 
-# workarea = '/home/acpaquette/repos/PDS-Pipelines/products/'
-workarea = '/home/acpaquette/repos/PDS-Pipelines/workarea/'
+scratch = '/scratch/pds_services/'
+workarea = os.path.join(scratch, 'workarea/')
+
 default_namespace = 'adampaquette_queue'
 
 pds_db = 'di_test'
 upc_db = 'upc_test'
 cluster_db = 'clusterjob_test'
-
-scratch = '/scratch/pds_services/'
 
 lock_obj = 'processes'
 
